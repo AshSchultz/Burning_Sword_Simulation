c503738d71ca3fedb94d082e553e4ec1aee9fa8a
AshSchultz <akmooreschultz@alaska.edu>
2024-12-10 14:29:49 -0900

    finished polishing project

--- scripts/fire_sword.gd
+++ scripts/fire_sword.gd
@@ -2,16 +2,7 @@ extends RigidBody3D
 
 var previous_state = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
-
 func _on_check_button_toggled(toggled_on: bool) -> void:
+	# Set the parameter on the fire and smoke particles
 	$Sword/Fire.process_material.turbulence_enabled = toggled_on
 	$Sword/Smoke.process_material.turbulence_enabled = toggled_on
